aa16e729a0bb0f92198a672fb5d5b453eef81590
Taha Babagil <tahababagil@gmail.com>
2025-03-10 00:57:46 +0000

    commit message

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def index():
     predicted_price = None
+    quality = None
     if request.method == "POST":
         try:
+            wineType = request.form.get("wineType", "red").lower()
             fixedAcidity = float(request.form.get("fixedAcidity", 0))
             volatileAcidity = float(request.form.get("volatileAcidity", 0))
             citricAcid = float(request.form.get("citricAcid", 0))
@@ -18,10 +20,12 @@ def index():
             pH = float(request.form.get("pH", 0))
             sulphates = float(request.form.get("sulphates", 0))
             alcohol = float(request.form.get("alcohol", 0))
-            predicted_price = predict_price(fixedAcidity, volatileAcidity, citricAcid, residualSugar, chlorides, freeSulfurDioxide, totalSulfurDioxide, density, pH, sulphates, alcohol)
+            predicted_price, quality = predict_price(wineType, fixedAcidity, volatileAcidity, citricAcid, residualSugar, chlorides, freeSulfurDioxide, totalSulfurDioxide, density, pH, sulphates, alcohol)
+            quality = round(quality,1)
+            predicted_price = round(predicted_price,2)
         except Exception as e:
             predicted_price = "Error: invalid input"
-    return render_template("index.html", price=predicted_price)
+    return render_template("index.html", price=predicted_price, quality=quality)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- regression.py
+++ regression.py
@@ -1,2 +1,34 @@
-def predict_price(fixedAcidity, volatileAcidity, citricAcid, residualSugar, chlorides, freeSulfurDioxide, totalSulfurDioxide, density, pH, sulphates, alcohol):
-    return 0.0
+import pandas as pd
+
+def predict_price(wineType : str, fixedAcidity, volatileAcidity, citricAcid, residualSugar, chlorides, freeSulfurDioxide, totalSulfurDioxide, density, pH, sulphates, alcohol):
+    
+    data = {
+        'fixed acidity': fixedAcidity,
+        'volatile acidity': volatileAcidity,
+        'citric acid': citricAcid,
+        'residual sugar': residualSugar,
+        'chlorides': chlorides,
+        'free sulfur dioxide': freeSulfurDioxide,
+        'total sulfur dioxide': totalSulfurDioxide,
+        'density': density,
+        'pH': pH,
+        'sulphates': sulphates,
+        'alcohol': alcohol
+    }
+
+    df = pd.DataFrame([data])
+
+    if wineType.strip() == "red":
+        model = pd.read_pickle("red_tree_quality.pkl")
+        rating = model.predict(df)[0]
+        prices = pd.read_pickle("red_price.pkl")
+        return prices.loc[(prices['Rating'] > rating - 0.2) & (prices['Rating'] < rating + 0.2)]['Price'].mean(), rating
+    else:
+        model = pd.read_pickle("white_tree_quality.pkl")
+        rating = model.predict(df)[0]
+        prices = pd.read_pickle("white_price.pkl")
+        print(prices.loc[(prices['Rating'] > rating - 0.5) & (prices['Rating'] < rating + 0.5)]['Price'])
+        return prices.loc[(prices['Rating'] > rating - 0.5) & (prices['Rating'] < rating + 0.5)]['Price'].mean(), rating
+
+
+# predict_price('white',0,0,0,0,0,0,0,0,0,0,0)
